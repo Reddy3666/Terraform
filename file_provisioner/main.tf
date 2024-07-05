586cae2dce7944bba11f46e3b797c6a71da7a81c
reddy3666 <dyr.linux@gmail.com>
2024-07-05 17:59:10 +0530

    adding file provisioner code

---
+++ file_provisioner/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/id_rsa.pub")
+
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-01376101673c89611"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = ["sg-08902816d2b63f5a6"]
+  key_name               = aws_key_pair.deployer.key_name
+
+  provisioner "file" {
+    source      = "config.file"
+    destination = "/tmp/config.file"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${path.module}/id_rsa")
+      host        = aws_instance.example.public_ip
+    }
+  }
+
+  tags = {
+    Name = "final-test"
+  }
+}
